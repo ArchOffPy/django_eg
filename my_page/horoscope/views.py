d2a530bc9b07f0378cd94a11dc7f38e93ba946fb
archoff <vipusk123@gmail.com>
2025-02-24 15:47:09 +0700

    added zodiak, urls and funcs into views

--- my_page/horoscope/views.py
+++ my_page/horoscope/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def aries(request):
+    return HttpResponse("Знак зодиака ОВЕН")
+
+def taurus(request):
+    return HttpResponse("Знак зодиака ТЕЛЕЦ")
+
+def gemini(request):
+    return HttpResponse("Знак зодиака БЛИЗНЕЦЫ")
+
+def cancer(request):
+    return HttpResponse("Знак зодиака РАК")
+
+def leo(request):
+    return HttpResponse("Знак зодиака ЛЕВ")
+
+def virgo(request):
+    return HttpResponse("Знак зодиака ДЕВА")
+
+def libra(request):
+    return HttpResponse("Знак зодиака ВЕСЫ")
+
+def scorpio(request):
+    return HttpResponse("Знак зодиака СКОРПИОН")
+
+def sagittarius(request):
+    return HttpResponse("Знак зодиака СТРЕЛЕЦ")
+
+def capricorn(request):
+    return HttpResponse("Знак зодиака КОЗЕРОГ")
+
+def aquarius(request):
+    return HttpResponse("Знак зодиака ВОДОЛЕЙ")
+
+def pisces(request):
+    return HttpResponse("Знак зодиака РЫБЫ")
