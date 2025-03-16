cdc0aefd9a9ccd9064bb9a002fabb9b6b9153f29
archoff <vipusk123@gmail.com>
2025-03-16 18:22:21 +0700

    update model in book app

--- bookshop/book_app/models.py
+++ bookshop/book_app/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=70)
     rating = models.IntegerField()
+    is_best_selling = models.BooleanField()
+    author = models.CharField(max_length=100, null=True)
 
     def __str__(self):
         return f"Книга: {self.title}\nРейтин: {self.rating}"
