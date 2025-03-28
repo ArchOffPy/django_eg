d3fc4ccacc0c60325e9719caec1d515eb23fee83
archoff <vipusk123@gmail.com>
2025-03-28 21:12:09 +0700

    add filter panel in django admin

--- movie_project/movie_app/admin.py
+++ movie_project/movie_app/admin.py
@@ -1,13 +1,43 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from .models import Movie
+from django.db.models import QuerySet
 
 # Register your models here.
+class RatingFilter(admin.SimpleListFilter):
+    title = 'Фильтр по рейтингу'
+    parameter_name = 'rating'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('0-29', 'Низкий'),
+            ('30-59', 'Средний'),
+            ('60-79', 'Высокий'),
+            ('80-100', 'Наилучший'),
+        ]
+
+    def queryset(self, request, queryset: QuerySet):
+        if self.value() == '0-29':
+            return queryset.filter(rating__lt=30)
+        if self.value() == '30-59':
+            return queryset.filter(rating__gte=30).filter(rating__lt=60)
+        if self.value() == '60-79':
+            return queryset.filter(rating__gte=60).filter(rating__lt=80)
+        if self.value() == '80-100':
+            return queryset.filter(rating__gte=80)
+        return queryset
+
+
+
+
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ['name', 'rating', 'year', 'rating_status']
-    list_editable = ['rating', 'year']
+    list_display = ['name', 'rating', 'year', 'currency', 'rating_status']
+    list_editable = ['rating', 'year', 'currency']
     ordering = ['rating', 'name']
-    list_per_page = 3
+    list_per_page = 10
+    actions = ['set_rubel', 'set_dollar']
+    search_fields = ['name__startswith', 'rating']
+    list_filter = ['name', RatingFilter, 'currency', ]
 
     @admin.display(ordering='rating', description='Rating')
     def rating_status(self, mov: Movie):
@@ -20,5 +50,17 @@ class MovieAdmin(admin.ModelAdmin):
 
         return 'Отличный фильм'
 
+    @admin.action(description="Установить валюту Рубль")
+    def set_rubel(self, request, qs: QuerySet):
+        qs.update(currency=Movie.RUB)
+
+    @admin.action(description="Установить валюту Доллар")
+    def set_dollar(self, request, qs: QuerySet):
+        count = qs.update(currency=Movie.USD)
+        self.message_user(
+            request,
+            f'Было обновлено {count} записей.',
+            messages.ERROR
+        )
 
 
