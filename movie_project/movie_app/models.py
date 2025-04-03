3e5cba44ac2f796e712a38c20efc847f9e934263
archoff <vipusk123@gmail.com>
2025-04-03 12:51:45 +0700

    add new model Director

--- movie_project/movie_app/models.py
+++ movie_project/movie_app/models.py
@@ -4,6 +4,16 @@ from django.utils.text import slugify
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
+class Director(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
 class Movie(models.Model):
     EUR = 'EUR'
     USD = 'USD'
